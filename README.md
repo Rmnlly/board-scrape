2d9e6d54fa6b05944234c4151703a30ed28aabd5
Raman <33904740+Rmnlly@users.noreply.github.com>
2020-02-16 16:47:54 -0500

    finished discovery, cannot deploy

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # board-scrape
+
 Data scraped about local ski resorts
+
 [![Netlify Status](https://api.netlify.com/api/v1/badges/25997a41-3571-483b-b32e-e17b00fbf8ae/deploy-status)](https://app.netlify.com/sites/board-scrape/deploys)
+
+You must run `netlify build` the first time you try building
+
+You can test the function at `http://localhost:34567/.netlify/functions/scrape-resort`
--- functions/scrape-resort.js
+++ functions/scrape-resort.js
@@ -12,25 +12,42 @@ const chromium = require("chrome-aws-lambda");
 
 exports.handler = async (event, context) => {
   // const { site } = event.queryStringParamaters;
+  let temp = null;
+  let browser = null;
+  try {
+    browser = await chromium.puppeteer.launch({
+      executablePath: await chromium.executablePath,
+      args: chromium.args,
+      headless: chromium.headless
+    });
 
-  const browser = await chromium.puppeteer.launch({
-    executablePath: await chromium.executablePath,
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    headless: chromium.headless
-  });
+    const page = await browser.newPage();
 
-  const page = await browser.newPage();
-  await page.goto("https://www.bluemountain.ca/");
-  const tempSelector =
-    "body > header > div.nav-right > div.condition-snippet-wrapper.opened > div.conditions-dropdown > div > div > div > div.conditions-weather.conditions-section > div.weather-wrapper > div.weather-current > div.weather-conditions > div > div > div > div.switchable-stat > span.switchable-stat-item.switchable-stat-metric";
-  const temp = await page.$eval(tempSelector, el => el.textContent.trim());
-  await browser.close();
+    await page.goto("https://www.bluemountain.ca/", {
+      waitUntil: ["domcontentloaded", "networkidle0"]
+    });
+
+    const tempSelector =
+      "body > header > div.nav-right > div.condition-snippet-wrapper > div.condition-snippet > span.condition-snippet-description";
+    temp = await page.$eval(tempSelector, el => el.textContent.trim());
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error
+      })
+    };
+  } finally {
+    // close browser
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      temp
+      temp: temp
     })
   };
 };
